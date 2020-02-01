32ba780e2e462196370139ac53053abf724333a8
Jesse Rafters <jesse.rafters@gmail.com>
2020-02-01 02:45:19 -0500

    Initial commit

---
+++ Shogi_GameServer/GameLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shogi_GameServer
+{
+    class GameLogic
+    {
+        public static void Update()
+        {
+            ThreadManager.UpdateMain();
+        }
+    }
+}
